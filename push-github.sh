e97d0ff7eebb23bf854178616a41124e3ba8b9a1
Cédric AUGUSTO <lemondedutravail803@gmail.com>
2026-03-12 10:43:42 +0100

    Update video pages with YouTube embeds

--- push-github.sh
+++ push-github.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 cd /c/Users/keqin/Desktop/portfolio
 
-# Supprimer l'historique et recommencer
-rm -rf .git
-git init
-
-# Configuration Git (après init)
 git config user.email "lemondedutravail803@gmail.com"
 git config user.name "Cédric AUGUSTO"
 
-# Ajouter uniquement les fichiers autorisés
 git add .
-git commit -m "Initial commit - Portfolio Cédric AUGUSTO"
-
-git branch -M main
-git remote add origin https://github.com/lemondedutravail803-lang/portfolio.git
-git push -u origin main --force
+git commit -m "Update video pages with YouTube embeds"
+git push -u origin main
